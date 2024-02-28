c9edb12e5c53e602ea7b06098f9bd81cd7eadda8
Breadtj <taking99@naver.com>
2024-02-28 20:45:24 +0900

    20240228_2045

---
+++ src/t3_homework/homework1.java
@@ -0,0 +1,15 @@
+package t3_homework;
+
+import java.util.Scanner;
+
+public class homework1 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int su = 0, i = 0, s = 0, h = 0, g = 0;
+		
+		
+		sc.close();
+	}
+
+}
